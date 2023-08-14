3848ea0f01be3bf4e7d7b0440f8897d2f08ae84a
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-08-14 10:59:59 +0900

    with ws

--- src/server.js
+++ src/server.js
@@ -1,1 +1,42 @@
-console.log("hello")
+const http = require("http");
+const WebSocket = require("ws");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+      default:
+        break;
+    }
+  });
+});
+
+server.listen(3000, () => console.log("✅ Server is Ready!"));
